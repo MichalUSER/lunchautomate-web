e668d3bc7595869ee0631ac213e19ce218c8968b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-05-27 16:17:17 +0200

    feat: Change env name

--- src/lib/utilities/config.ts
+++ src/lib/utilities/config.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
-import { PUBLIC_URL } from '$env/static/public';
+import { PUBLIC_API_URL } from '$env/static/public';
 
-const URL = dev ? 'http://127.0.0.1:8000' : PUBLIC_URL;
+const URL = dev ? 'http://127.0.0.1:8000' : PUBLIC_API_URL;
 
 export { URL };
